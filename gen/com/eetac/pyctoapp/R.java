b1f85f2c101087cb23f513bf715b04c43606177d
Anna Hurtado <anna.hurtado.borras@gmail.com>
2014-05-26 20:20:16 +0200

    Bugs arreglados.

--- gen/com/eetac/pyctoapp/R.java
+++ gen/com/eetac/pyctoapp/R.java
@@ -8,7 +8,16 @@
 package com.eetac.pyctoapp;
 
 public final class R {
+    public static final class array {
+        /**  Navigation Drawer 
+         */
+        public static final int planets_array=0x7f070000;
+        public static final int titles_array=0x7f070001;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int checkbox_light_color=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -18,27 +27,140 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+        public static final int navigation_drawer_width=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_search=0x7f020000;
+        public static final int button_gray_border=0x7f020001;
+        public static final int button_login_border=0x7f020002;
+        public static final int button_register_border=0x7f020003;
+        public static final int checkbox_light_btn_check_holo_dark=0x7f020004;
+        public static final int checkbox_light_btn_check_off_disabled_focused_holo_dark=0x7f020005;
+        public static final int checkbox_light_btn_check_off_disabled_holo_dark=0x7f020006;
+        public static final int checkbox_light_btn_check_off_focused_holo_dark=0x7f020007;
+        public static final int checkbox_light_btn_check_off_holo_dark=0x7f020008;
+        public static final int checkbox_light_btn_check_off_pressed_holo_dark=0x7f020009;
+        public static final int checkbox_light_btn_check_on_disabled_focused_holo_dark=0x7f02000a;
+        public static final int checkbox_light_btn_check_on_disabled_holo_dark=0x7f02000b;
+        public static final int checkbox_light_btn_check_on_focused_holo_dark=0x7f02000c;
+        public static final int checkbox_light_btn_check_on_holo_dark=0x7f02000d;
+        public static final int checkbox_light_btn_check_on_pressed_holo_dark=0x7f02000e;
+        public static final int custom_checkbox=0x7f02000f;
+        public static final int drawer_shadow=0x7f020010;
+        public static final int header=0x7f020011;
+        public static final int header_border=0x7f020012;
+        public static final int ic_action_accept=0x7f020013;
+        public static final int ic_action_collection=0x7f020014;
+        public static final int ic_action_person=0x7f020015;
+        public static final int ic_action_picture=0x7f020016;
+        public static final int ic_action_upload=0x7f020017;
+        public static final int ic_drawer=0x7f020018;
+        public static final int ic_launcher=0x7f020019;
+        public static final int login_background=0x7f02001a;
+        public static final int logo_blanc=0x7f02001b;
+        public static final int sample_0=0x7f02001c;
+        public static final int sample_1=0x7f02001d;
+        public static final int sample_2=0x7f02001e;
+        public static final int sample_3=0x7f02001f;
+        public static final int sample_4=0x7f020020;
+        public static final int sample_5=0x7f020021;
+        public static final int sample_6=0x7f020022;
+        public static final int sample_7=0x7f020023;
+        public static final int thumbnail_border=0x7f020024;
+        public static final int user_profile=0x7f020025;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int LoginButton=0x7f0a0005;
+        public static final int Password=0x7f0a0004;
+        public static final int Username=0x7f0a0003;
+        public static final int action_settings=0x7f0a0021;
+        public static final int alteraid_logo=0x7f0a000b;
+        public static final int alteraid_logo_text=0x7f0a000a;
+        public static final int browse_button=0x7f0a001c;
+        public static final int content_frame=0x7f0a000d;
+        public static final int contest_photo=0x7f0a0014;
+        public static final int create_account_button=0x7f0a0008;
+        public static final int divider=0x7f0a0019;
+        public static final int divider1=0x7f0a0013;
+        public static final int drawer_layout=0x7f0a000c;
+        public static final int fragment_text=0x7f0a0010;
+        public static final int gridview=0x7f0a000f;
+        public static final int header_brand=0x7f0a0020;
+        public static final int layout=0x7f0a0000;
+        public static final int left_drawer=0x7f0a000e;
+        public static final int login_button=0x7f0a0007;
+        public static final int login_button_layout=0x7f0a0002;
+        public static final int logo=0x7f0a0001;
+        public static final int relative_header=0x7f0a001e;
+        public static final int rememberCheckBox=0x7f0a0006;
+        public static final int scroll=0x7f0a0017;
+        public static final int terms_text=0x7f0a001a;
+        public static final int terms_title=0x7f0a0018;
+        public static final int thumbnail=0x7f0a0015;
+        public static final int thumbnail_image=0x7f0a0016;
+        public static final int upload_button=0x7f0a001d;
+        public static final int upload_photo=0x7f0a001b;
+        public static final int user_email=0x7f0a0012;
+        public static final int user_icon=0x7f0a0011;
+        public static final int user_logo=0x7f0a001f;
+        public static final int version_text=0x7f0a0009;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int drawer_list_item=0x7f030002;
+        public static final int fragment_gallery=0x7f030003;
+        public static final int fragment_map=0x7f030004;
+        public static final int fragment_planet=0x7f030005;
+        public static final int fragment_profile=0x7f030006;
+        public static final int fragment_terms=0x7f030007;
+        public static final int fragment_upload=0x7f030008;
+        public static final int header=0x7f030009;
+        public static final int title_drawer=0x7f03000a;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int global=0x7f090000;
+        public static final int main=0x7f090001;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_example=0x7f06000f;
+        public static final int action_settings=0x7f060002;
+        public static final int action_websearch=0x7f060013;
+        public static final int app_name=0x7f060000;
+        public static final int browse_button=0x7f060016;
+        public static final int contest_photo=0x7f06001b;
+        public static final int create_account_text=0x7f060007;
+        public static final int drawer_close=0x7f060012;
+        public static final int drawer_open=0x7f060011;
+        public static final int enter_password_hint=0x7f060004;
+        /**  CreateAccountActivity & LoginActivity strings 
+         */
+        public static final int enter_username_hint=0x7f060003;
+        public static final int hello_world=0x7f060001;
+        public static final int image_header=0x7f060010;
+        public static final int login=0x7f060005;
+        public static final int navigation_drawer_close=0x7f06000e;
+        public static final int navigation_drawer_open=0x7f06000d;
+        public static final int ok_button=0x7f06001a;
+        /**  Pycto Terms fragment 
+         */
+        public static final int pycto_terms=0x7f060017;
+        public static final int pycto_terms_title=0x7f060018;
+        public static final int remember_me=0x7f060006;
+        public static final int title_activity_main=0x7f060009;
+        public static final int title_section1=0x7f06000a;
+        public static final int title_section2=0x7f06000b;
+        public static final int title_section3=0x7f06000c;
+        public static final int upload_button=0x7f060015;
+        /**  Upload photo fragment 
+         */
+        public static final int upload_photo=0x7f060014;
+        /**  Profile fragment 
+         */
+        public static final int user_email=0x7f060019;
+        public static final int user_password_incorrect=0x7f060008;
     }
     public static final class style {
         /** 
@@ -62,10 +184,12 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+        public static final int CheckBoxCheckBox_light=0x7f080002;
+        public static final int CheckBox_light=0x7f080003;
     }
 }
